af2bb2ba7ba3acd4aae3ec6f40e94b3005b6579e
Y0legA <myad_dib@mail.ru>
2022-05-24 20:33:38 +0300

    24.05.22

--- fmh/UI/Modules/MainScreenViewController/Cell/CellRequests/CellRequests.swift
+++ fmh/UI/Modules/MainScreenViewController/Cell/CellRequests/CellRequests.swift
@@ -9,106 +9,120 @@ import UIKit
 
 class CellRequests: UITableViewCell {
     
-    let taskThemeLabel = UILabel(text: "тема", font: .avenirNextDemiBold20())
-    let taskDescription = UILabel(text: "заменить матрац", font: .avenirNextDemiBold20())
-    let patientLabel = UILabel(text: "пациент", font: .avenirNextDemiBold20())
-    let patientName = UILabel(text: "А.Н.Акопова", font: .avenirNextDemiBold20())
-    let executorLabel = UILabel(text: "исполнитель", font: .avenirNextDemiBold20())
-    let executorName = UILabel(text: "М.Л.Павлов", font: .avenirNextDemiBold20())
-    let taskDateLabel = UILabel(text: "10.19.22", font: .avenirNextDemiBold20())
-    let taskDate = UILabel(text: "10.19.22", font: .avenirNextDemiBold20())
-    let tasklTime = UILabel(text: "9:00", font: .avenirNextDemiBold14())
+    private let taskThemeLabel = UILabel(text: "тема", font: .avenirNextDemiBold20())
+    private let taskDescription = UILabel(text: "заменить матрац", font: .avenirNextDemiBold20())
+    private let patientLabel = UILabel(text: "пациент", font: .avenirNextDemiBold20())
+    private let patientName = UILabel(text: "А.Н.Акопова", font: .avenirNextDemiBold20())
+    private let executorLabel = UILabel(text: "исполнитель", font: .avenirNextDemiBold20())
+    private let executorName = UILabel(text: "М.Л.Павлов", font: .avenirNextDemiBold20())
+    private let taskDateLabel = UILabel(text: "10.19.22", font: .avenirNextDemiBold20())
+    private let taskDate = UILabel(text: "10.19.22", font: .avenirNextDemiBold20())
+    private let tasklTime = UILabel(text: "9:00", font: .avenirNextDemiBold14())
     
-    var taskColor = CGColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1)
-    var separateLine = UIColor.red
+    private var taskColor = CGColor(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+    private var separateLineColor = UIColor.red
+    let lineFirst = UIView()
+    let lineSecond = UIView()
+    let linethird = UIView()
+    let lineFourth = UIView()
     
-    let openTask: UIButton = {
-        let button = UIButton()
-        button.setBackgroundImage(UIImage(named: "Vector"), for: .normal)
-        button.tintColor = .black
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
+    private let openTask = UIButton()
+    
+    private let cellView = UIView()
+    private let resultView = UIView()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.selectionStyle = .none
         self.openTask.addTarget(self, action: #selector(openTaskAction), for: .touchUpInside)
+        configureCell()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func configureCell(borderColor: CGColor, separateLine: UIColor) {
-        let cellView: UIView = {
-            let cell = UIView()
-            
-            cell.frame = CGRect(x: 20, y: 20, width: self.frame.width - 40, height: self.frame.height - 30)
-            cell.layer.cornerRadius = 10
-            cell.layer.borderWidth = 2
-            cell.layer.borderColor =  borderColor
-            
-            func separateLines(frame: CGRect, colour: UIColor) -> UIView {
-                let viewSeparatorLine = UIView(frame: frame)   // разделитель ячеек
-                viewSeparatorLine.backgroundColor = colour
-                return viewSeparatorLine
-            }
-            
-            self.contentView.addSubview(separateLines(frame: CGRect(x: 30, y: 63, width: self.contentView.frame.size.width - 60, height: 2), colour: separateLine))
-            self.contentView.addSubview(separateLines(frame: CGRect(x: 30, y: 99, width: self.contentView.frame.size.width - 60, height: 2), colour: separateLine))
-            self.contentView.addSubview(separateLines(frame: CGRect(x: 30, y: 131, width: self.contentView.frame.size.width - 60, height: 2), colour: separateLine))
-            self.contentView.addSubview(separateLines(frame: CGRect(x: 30, y: 165, width: self.contentView.frame.size.width - 60, height: 2), colour: separateLine))
-            return cell
-        }()
+    private func configureCell() {
+        self.backgroundColor = UIColor(white: 0, alpha: 0)
         
-        let themeStackView = UIStackView(arrangedSubviews: [taskThemeLabel, taskDescription], axis: .horizontal, spacing: 10, distribution: .equalSpacing)
-        cellView.addSubview(themeStackView)
+        self.addSubview(self.resultView)
+        self.resultView.backgroundColor = .white
+
+        self.resultView.addSubview(self.cellView)
+        self.cellView.layer.cornerRadius = 10
+        self.cellView.layer.borderWidth = 2
+        self.cellView.layer.borderColor =  taskColor
+        self.cellView.backgroundColor = .white
+        
+        self.openTask.setBackgroundImage(UIImage(named: "Vector"), for: .normal)
+        
+        let themeStackView = UIStackView(arrangedSubviews: [self.taskThemeLabel, self.taskDescription], axis: .horizontal, spacing: 10, distribution: .equalSpacing)
+        self.cellView.addSubview(themeStackView)
         NSLayoutConstraint.activate([
-            themeStackView.topAnchor.constraint(equalTo: cellView.topAnchor, constant: 10),
-            themeStackView.leadingAnchor.constraint(equalTo: cellView.leadingAnchor, constant: 10),
-            themeStackView.trailingAnchor.constraint(equalTo: cellView.trailingAnchor, constant: -10),
+            themeStackView.topAnchor.constraint(equalTo: self.cellView.topAnchor, constant: 5),
+            themeStackView.leadingAnchor.constraint(equalTo: self.cellView.leadingAnchor, constant: 10),
+            themeStackView.trailingAnchor.constraint(equalTo: self.cellView.trailingAnchor, constant: -10),
             themeStackView.heightAnchor.constraint(equalToConstant: 30)
         ])
         
-        let patientStackView = UIStackView(arrangedSubviews: [patientLabel, patientName], axis: .horizontal, spacing: 10, distribution: .equalSpacing)
-        cellView.addSubview(patientStackView)
+        let patientStackView = UIStackView(arrangedSubviews: [self.patientLabel, self.patientName], axis: .horizontal, spacing: 10, distribution: .equalSpacing)
+        self.cellView.addSubview(patientStackView)
         NSLayoutConstraint.activate([
             patientStackView.topAnchor.constraint(equalTo: themeStackView.bottomAnchor, constant: 5),
-            patientStackView.leadingAnchor.constraint(equalTo: cellView.leadingAnchor, constant: 10),
-            patientStackView.trailingAnchor.constraint(equalTo: cellView.trailingAnchor, constant: -10),
+            patientStackView.leadingAnchor.constraint(equalTo: self.cellView.leadingAnchor, constant: 10),
+            patientStackView.trailingAnchor.constraint(equalTo: self.cellView.trailingAnchor, constant: -10),
             patientStackView.heightAnchor.constraint(equalToConstant: 30)
         ])
         
-        let executorStackView = UIStackView(arrangedSubviews: [executorLabel, executorName], axis: .horizontal, spacing: 10, distribution: .equalSpacing)
-        cellView.addSubview(executorStackView)
+        let executorStackView = UIStackView(arrangedSubviews: [self.executorLabel, self.executorName], axis: .horizontal, spacing: 10, distribution: .equalSpacing)
+        self.cellView.addSubview(executorStackView)
         NSLayoutConstraint.activate([
             executorStackView.topAnchor.constraint(equalTo: patientStackView.bottomAnchor, constant: 5),
-            executorStackView.leadingAnchor.constraint(equalTo: cellView.leadingAnchor, constant: 10),
-            executorStackView.trailingAnchor.constraint(equalTo: cellView.trailingAnchor, constant: -10),
+            executorStackView.leadingAnchor.constraint(equalTo: self.cellView.leadingAnchor, constant: 10),
+            executorStackView.trailingAnchor.constraint(equalTo: self.cellView.trailingAnchor, constant: -10),
             executorStackView.heightAnchor.constraint(equalToConstant: 30)
         ])
         
-        let planeDateStackView = UIStackView(arrangedSubviews: [taskDateLabel, taskDate, tasklTime], axis: .horizontal, spacing: 10, distribution: .equalSpacing)
-        cellView.addSubview(planeDateStackView)
+        let planeDateStackView = UIStackView(arrangedSubviews: [self.taskDateLabel, self.taskDate, self.tasklTime], axis: .horizontal, spacing: 10, distribution: .equalSpacing)
+        self.cellView.addSubview(planeDateStackView)
         NSLayoutConstraint.activate([
             planeDateStackView.topAnchor.constraint(equalTo: executorStackView.bottomAnchor, constant: 5),
-            planeDateStackView.leadingAnchor.constraint(equalTo: cellView.leadingAnchor, constant: 10),
-            planeDateStackView.trailingAnchor.constraint(equalTo: cellView.trailingAnchor, constant: -10),
+            planeDateStackView.leadingAnchor.constraint(equalTo: self.cellView.leadingAnchor, constant: 10),
+            planeDateStackView.trailingAnchor.constraint(equalTo: self.cellView.trailingAnchor, constant: -10),
             planeDateStackView.heightAnchor.constraint(equalToConstant: 30)
         ])
         
-        cellView.addSubview(openTask)
+        self.cellView.addSubview(self.openTask)
+        self.openTask.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            openTask.topAnchor.constraint(equalTo: planeDateStackView.bottomAnchor, constant: 8),
-            openTask.leadingAnchor.constraint(equalTo: cellView.leadingAnchor, constant: self.bounds.width / 2 - 20),
-            openTask.widthAnchor.constraint(equalToConstant: 15),
+            self.openTask.topAnchor.constraint(equalTo: planeDateStackView.bottomAnchor, constant: 5),
+            self.openTask.centerXAnchor.constraint(equalTo: self.cellView.centerXAnchor),
+            self.openTask.widthAnchor.constraint(equalToConstant: 15),
         ])
         
-        self.addSubview(cellView)
+        let separateLines = [lineFirst, lineSecond, linethird, lineFourth]
+        for line in separateLines {
+        self.cellView.addSubview(line)
+        line.backgroundColor = separateLineColor
+        }
+                                 
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.resultView.frame = CGRect(x: 20, y: 0, width: self.bounds.width - 40, height: self.bounds.height)
+        self.cellView.frame = CGRect(x: 20, y: 20, width: self.resultView.bounds.width - 40, height: self.resultView.bounds.height - 40)
+        self.lineFirst.frame = setSepatateLines(y: 37)
+        self.lineSecond.frame = setSepatateLines(y: 75)
+        self.linethird.frame = setSepatateLines(y: 105)
+        self.lineFourth.frame = setSepatateLines(y: 140)
     }
     
-    @objc func openTaskAction() {
-           print("open task")
-       }
+    private func setSepatateLines(y: Double) -> CGRect {
+    return CGRect(x: self.cellView.bounds.minX + 10, y: y , width: self.cellView.bounds.width - 20, height: 1)
+    }
     
+    @objc private  func openTaskAction() {
+        print("open task")
+    }
 }
+
--- fmh/UI/Modules/MainScreenViewController/MainScreenViewController.swift
+++ fmh/UI/Modules/MainScreenViewController/MainScreenViewController.swift
@@ -7,17 +7,20 @@
 
 import UIKit
 
+
 final class MainScreenViewController: UIViewController {
     
     // MARK: - Временные переменные..
     
-    let text = "text of news -- text of news -- text of news -- text of news -- text of news -- text of news -- text of news -- text of news -- text of news -- text of news -- text of news -- text of news -- text of news -- text of news -- text of news -- text of news -- text of news -- text of news -- text of news -- text of news"
+    let testText = "text of news -- text of news -- text of news -- text of news -- text of news -- text of news -- text of news -- text of news -- text of news -- text of news -- text of news -- text of news -- text of news -- text of news -- text of news -- text of news -- text of news -- text of news -- text of news -- text of news"
     
-    var buttonOpenNewIsActive = false
+    
     
     // MARK: - External vars
     
     var presenter: MainScreenPresenterDelegate?
+    
+    // MARK:: - Internal vars
     
     private let idNewsHeader  = "idNewsHeader"
     private let idNewsFooter = "idNewsFooter"
@@ -27,34 +30,28 @@ final class MainScreenViewController: UIViewController {
     private let cellNewsId = "cellNews"
     private let cellRequestsId = "cellRequests"
     private let titles = ["Новости", "Просьбы"]
-    private let bottomButtons = ["ВСЕ НОВОСТИ", "ВСЕ ПРОСЬБЫ"]
-    private let showMore = "ПОКАЗАТЬ ЕЩЕ"
     
-    var cellNewsArray = 1
-    var cellRequestsArray = 5
+    let handlerView: HeaderView? = nil
     
-    private let tableview: UITableView = {
-        let tableview = UITableView()
-        tableview.translatesAutoresizingMaskIntoConstraints = false
-        return tableview
-    }()
+    var buttonOpenNewIsActive = false
+    
+    var cellNewsArrayCount = 2
+    var cellRequestsArrayCount = 3
+    
+    private let tableview = UITableView()
+    
+    var newsText: ((String) -> ())? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureMain()
-        
     }
     
     private func configureMain() {
         self.view.addSubview(UIImageView(image: UIImage(named: "MainBackground")))
         self.tableview.backgroundColor = UIColor(white: 0.0, alpha: 0.0)
-        
-        self.tableview.register(HeaderForCellNews.self, forHeaderFooterViewReuseIdentifier: idNewsHeader)
-        self.tableview.register(FooterForCellNews.self, forHeaderFooterViewReuseIdentifier: idNewsFooter)
-        self.tableview.register(HeaderForCellRequests.self, forHeaderFooterViewReuseIdentifier: idRequestHeader)
-        self.tableview.register(FooterForCellRequests.self, forHeaderFooterViewReuseIdentifier: idRequestFooter)
-        
+
         self.tableview.register(CellNews.self, forCellReuseIdentifier: cellNewsId)
         self.tableview.register(CellRequests.self, forCellReuseIdentifier: cellRequestsId)
         
@@ -62,10 +59,10 @@ final class MainScreenViewController: UIViewController {
         self.tableview.dataSource = self
         
         setConstraints()
+        self.tableview.rowHeight = UITableView.automaticDimension
         
         self.title = "NAV_BAR"  //Временнный тайтл
     }
-    
 }
 
 // MARK: - MainScreenViewControllerDelegate, UITableViewDelegate, UITableViewDataSourcee
@@ -78,33 +75,24 @@ extension MainScreenViewController: MainScreenViewControllerDelegate, UITableVie
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch section {
-        case 0: return cellNewsArray
-        case 1: return cellRequestsArray
+        case 0: return cellNewsArrayCount
+        case 1: return cellRequestsArrayCount
         default: break
         }
         return 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        self.tableview.showsVerticalScrollIndicator = false
-        
-        let taskColor =  CGColor(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
-        let separateLine = UIColor.red
-        
         switch indexPath.section {
         case 0:
             let cell = tableView.dequeueReusableCell(withIdentifier: cellNewsId, for: indexPath) as! CellNews
-            cell.openNewsButton.addTarget(self, action: #selector(openNews), for: .touchUpInside)
-            
-            self.buttonOpenNewIsActive ? (cell.newsTextLabel.text = text) : (cell.newsTextLabel.text = "")
-            
-            cell.newsTextLabel.numberOfLines = 0
-            cell.newsTextLabel.sizeToFit()
-            cell.contentView.isHidden = true
+            self.buttonOpenNewIsActive ? ( cell.updateText(text: testText, image: "ArrowUp")) : ( cell.updateText(text: "", image: "ArrowDown"))
+            cell.layer.borderWidth = 1
+            cell.layer.borderColor =  #colorLiteral(red: 0.8901960784, green: 0.8901960784, blue: 0.8901960784, alpha: 1)
             return cell
         case 1:
             let cell = tableView.dequeueReusableCell(withIdentifier: cellRequestsId, for: indexPath) as! CellRequests
-            cell.configureCell(borderColor: taskColor, separateLine: separateLine)
+            
             return cell
         default: break
         }
@@ -113,51 +101,45 @@ extension MainScreenViewController: MainScreenViewControllerDelegate, UITableVie
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         switch indexPath.section {
-        case 0: return  UITableView.automaticDimension
-        case 1: return 200
+        case 0:
+            return UITableView.automaticDimension
+        case 1:
+            return 200
         default: break
         }
         return 0
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        buttonOpenNewIsActive = !buttonOpenNewIsActive
+        tableView.reloadRows(at: [indexPath], with: .automatic)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        100
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        40
+    }
+    
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        switch section {
-        case 0:
-            let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: idNewsHeader) as! HeaderForCellNews
-            header.headerConfigure(name: titles[0], section: section, width: self.view.bounds.width, heigth: 40)
-            header.headerBottomButton.setTitle(bottomButtons[0], for: .normal)
-            return header
-        case 1:
-            let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: idRequestHeader) as! HeaderForCellRequests
-            header.headerConfigure(name: titles[1], section: section, width: self.view.bounds.width, heigth: 40)
-            header.headerBottomButton.setTitle(bottomButtons[1], for: .normal)
-            return header
-        default: break
+        guard let headerType = HeaderView.HeaderType(rawValue: section) else { return nil }
+        let header = HeaderView(type: headerType)
+        header.pressUpButton = { (type, tag) in
+            print(type)
+            print(tag)
         }
-        return UIView()
+        header.pressDownButton = { type in
+            print(type)
+        }
+        return header
     }
     
     func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-        switch section {
-        case 0:
-            let footer = tableView.dequeueReusableHeaderFooterView(withIdentifier: idNewsFooter) as! FooterForCellNews
-            footer.footerConfigure(width: self.view.bounds.width, heigth: 40)
-            footer.footerButton.setTitle(showMore, for: .normal)
-            return footer
-        case 1:
-            let footer = tableView.dequeueReusableHeaderFooterView(withIdentifier: idRequestFooter) as! FooterForCellRequests
-            footer.footerConfigure(width: self.view.bounds.width, heigth: 40)
-            footer.footerButton.setTitle(showMore, for: .normal)
-            return footer
-        default: break
-        }
-        return UIView()
-    }
-    
-    @objc func openNews() {
-        print("show current new")
-        buttonOpenNewIsActive = !buttonOpenNewIsActive
-        tableview.reloadData()
+        guard let footerType = FooterView.FooterType(rawValue: section) else { return nil }
+        let footer = FooterView(type: footerType)
+        return footer
     }
     
 }
@@ -165,12 +147,14 @@ extension MainScreenViewController: MainScreenViewControllerDelegate, UITableVie
 extension MainScreenViewController {
     
     func setConstraints() {
-        self.view.addSubview(tableview)
+        self.view.addSubview(self.tableview)
+        tableview.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            tableview.topAnchor.constraint(equalTo: view.topAnchor, constant: 10),
-            tableview.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            tableview.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            tableview.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10)
+            self.tableview.topAnchor.constraint(equalTo: view.topAnchor, constant: 10),
+            self.tableview.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            self.tableview.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            self.tableview.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10)
         ])
     }
 }
+
